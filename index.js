e6b10487353bb64c79c173b6f9c00e74dc737750
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-10-25 21:16:19 +0300

    hotfix

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ const  bot  =  new  TelegramBot ( token ,  { polling : true } ) ;
 
   const asdfg = async () => {
     const chatId = 505344184;
-    let driver = new Builder()
-    
 
+    const options = new chrome.Options().addArguments('--headless');
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+
+    let driver = new Builder()
     .forBrowser(Browser.CHROME)
-    .setChromeOptions(new chrome.Options().addArguments('--headless'))
+    .setChromeOptions(options)
     .build();
     
     await driver.get('https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E+&newwindow=1&sxsrf=ALiCzsY543bfT8TEbKLsQlwhYsztUVCQEA%3A1666206923019&ei=y0xQY_5mr7fFzw-r_YboCQ&ved=0ahUKEwi--JK-gO36AhWvW_EDHau-AZ0Q4dUDCA8&uact=5&oq=%D0%BA%D1%83%D1%80%D1%81+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E+&gs_lcp=Cgdnd3Mtd2l6EAMyCggAEIAEEEYQggIyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgjEOoCECc6BAgjECc6BAgAEEM6CQgjECcQRhCCAjoKCAAQgAQQhwIQFDoPCAAQgAQQhwIQFBBGEIICSgQIQRgASgQIRhgAUMgNWIBCYL9DaAFwAXgAgAHCAogBhSOSAQgwLjIuMTYuMpgBAKABAbABCsABAQ&sclient=gws-wiz');
