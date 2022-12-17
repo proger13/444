e3b185a23c59c9c2351d244c539883823bb52f2a
Amal Abdullaev <amal.abdullaev.96@mail.ru>
2022-12-17 23:25:26 +0400

    update

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const  bot  =  new  TelegramBot ( token ,  { polling : true } ) ;
   const asdfg = async () => {
     const chatId = 505344184;
 
-    const options = new chrome.Options().addArguments('--headless');
-    options.addArguments("--no-sandbox");
-    options.addArguments("--disable-dev-shm-usage");
+    const options = new chrome.Options()
 
     let driver = new Builder()
     .forBrowser(Browser.CHROME)
@@ -22,18 +20,18 @@ const  bot  =  new  TelegramBot ( token ,  { polling : true } ) ;
     
     await driver.get('https://www.finam.ru/quote/mosbirzha-valyutnyj-rynok/usdrubtod-usd-rub/');
     const kursusd = await driver.findElement(By.css('#finfin-local-plugin-quote-item-review-price-container')).getText()
-    bot.sendMessage(chatId, kursusd);
+    bot.sendMessage(chatId, 'USD: ' + kursusd);
 
     await driver.get('https://www.finam.ru/quote/mosbirzha-valyutnyj-rynok/eurrubtod-eur-rub/');
     const kurseur = await driver.findElement(By.css('#finfin-local-plugin-quote-item-review-price-container')).getText()
-    bot.sendMessage(chatId, kurseur);
+    bot.sendMessage(chatId, 'EURO: ' + kurseur);
 
-    await driver.get('https://www.bybit.com/fiat/trade/express/home');
-    await driver.findElement(By.css('#express__select-wrap .by-popover__el')).click()
-    await driver.findElement(By.css('.express-center-select__search-input .by-input__inner')).sendKeys('RUB')
-    await driver.findElement(By.css('.express-center-select__list-wrapper')).click()
-    const kursbybit = await driver.findElement(By.css('.express-create-modal__transfer-icon')).getText()
-    console.log(kursbybit);
+    // await driver.get('https://www.bybit.com/fiat/trade/express/home');
+    // await driver.findElement(By.css('#express__select-wrap .by-popover__el')).click()
+    // await driver.findElement(By.css('.express-center-select__search-input .by-input__inner')).sendKeys('RUB')
+    // await driver.findElement(By.css('.express-center-select__list-wrapper')).click()
+    // const kursbybit = await driver.findElement(By.css('.express-create-modal__transfer-icon')).getText()
+    // console.log(kursbybit);
     driver.close()
   }
   
